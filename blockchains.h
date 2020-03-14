efa13cc2e4a72ea82755c1bcea8f180089a99138
tristanlorriaux <57456860+tristanlorriaux@users.noreply.github.com>
2020-03-14 18:43:32 +0100

    Create blockchains.h

---
+++ blockchains.h
@@ -0,0 +1,31 @@
+#include "openssl/sha.h"
+#include "openssl/crypto.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct{
+    char message;
+    char exp;
+    char dest;
+}Donnee;
+
+struct bloc{
+    char precHash[SHA256_DIGEST_LENGTH];
+    Donnee donnee;
+    struct bloc *lien
+}*Genesis;
+
+void ajout_bloc(Donnee message);
+void verifyChain(void);
+void alterNthBlock(int n, int newData);
+void hackChain(void);
+unsigned char *toString(struct bloc blocks);
+void hashPrinter(unsigned char hash[], int length);
+int hashCompare(unsigned char *str1, unsigned char *str2);
+void printBloc(struct bloc *blocs);
+void printAllBlock(void);
+
+
+
