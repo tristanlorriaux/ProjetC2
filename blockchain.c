78604aa9f9ca93b00909db349c334f7b6b5de6a8
lucasmaisonnave <56760454+lucasmaisonnave@users.noreply.github.com>
2020-03-28 17:33:50 +0100

    Update blockchain.c

--- blockchain.c
+++ blockchain.c
@@ -10,8 +10,8 @@ void ajout_block(Donnee* message){
         Genesis->lien = NULL;
         Genesis->index = 0;
         Genesis->nonce = 0;
-        char* gene_hex[HASH_HEX_SIZE];
-        Hex_Hash("", gene_hex);
+        char gene_hex[HASH_HEX_SIZE];
+        strcpy(Genesis->Hash, "");
         strcpy(Genesis->Hash, gene_hex); 
         strcpy(Genesis->precHash, "");
         init_Data(Genesis->donnee);
@@ -21,6 +21,7 @@ void ajout_block(Donnee* message){
     {
         currentbloc=currentbloc->lien;
     }
+
     
     struct bloc *nouv_bloc = (struct bloc *)malloc(sizeof(struct bloc));  
     nouv_bloc->donnee = (Donnee*)malloc(sizeof(Donnee));  
@@ -30,17 +31,21 @@ void ajout_block(Donnee* message){
     nouv_bloc->index = currentbloc->index + 1;    
     strcpy(nouv_bloc->precHash, currentbloc->Hash);
     calculHash(nouv_bloc);
-
-    char* bloc_string[BLOCK_STR_SIZE];
-    toString(nouv_bloc, bloc_string);
-    printf("bloc_string 2 : %s\n", bloc_string);
-
-
+    /*char hash[HASH_SIZE]= {0};
+    char hash_hex[HASH_HEX_SIZE]= {0};
+    char bloc_string[BLOCK_STR_SIZE]= {0};
     
+    //hash256(hash, toString(nouv_bloc, bloc_string));
+    Hex_Hash(nouv_bloc, hash_hex);
+    printf("Hash calcule hors fonction : %s\n", hash_hex);
+    //hash256(hash, toString(nouv_bloc, bloc_string));
+    Hex_Hash(nouv_bloc, hash_hex);
+    printf("Hash calcule hors fonction : %s\n", hash_hex);
+    printf("Hash du bloc hors fonction : %s\n", nouv_bloc->Hash);*/
+    printf("yo\n");
     if(IsValidBlock(nouv_bloc,currentbloc))
     {   
         currentbloc->lien = nouv_bloc;
-        printf("yo\n");
         
     }
     else
@@ -84,17 +89,21 @@ char *toString(struct bloc *bloc, char *str)
     if(bloc == NULL)
         return NULL;    
     
-    char b[3];
-    char c[32];
+    char block_string[BLOCK_STR_SIZE] = {0};
+    char b[3]= {0};
+    char c[32]= {0};
     sprintf(b, "%d", bloc->index);    
-    strcpy(str, b);
-    strcat(str, bloc->precHash);
-    strcat(str, bloc->donnee->date);
-    strcat(str, bloc->donnee->dest);
-    strcat(str, bloc->donnee->exp);
-    strcat(str, bloc->donnee->message);
-    sprintf(c, "%ld", bloc->nonce);
-    strcat(str, c);
+    strcpy(block_string, b);
+    strcat(block_string, bloc->precHash);
+    strcat(block_string, bloc->donnee->date);
+    strcat(block_string, bloc->donnee->dest);
+    strcat(block_string, bloc->donnee->exp);
+    strcat(block_string, bloc->donnee->message);
+    sprintf(c, "%d", bloc->nonce);
+    strcat(block_string, c);
+
+    
+    strcpy(str, block_string);
     return str;
 }
 
@@ -152,7 +161,7 @@ void init_Data(Donnee* data)
 
 bool HashMatchesDifficulty(char Hex[HASH_HEX_SIZE])
 {
-    char binaryHash[BINARY_SIZE] = "";
+    char binaryHash[BINARY_SIZE] =  {0};
     hexToBinary(Hex, binaryHash);
     printf("bin : %s\n", binaryHash);
 
@@ -163,7 +172,6 @@ bool HashMatchesDifficulty(char Hex[HASH_HEX_SIZE])
     
     char *checker = NULL;
     checker = strstr(binaryHash, prefix);
-
     return checker == binaryHash;
 }
 
@@ -242,7 +250,7 @@ void hash256(unsigned char *output, const char *input)
 {
 
     size_t length = strlen(input);
-    unsigned char md[HASH_SIZE];
+    unsigned char md[HASH_SIZE] = {0};
     SHA256((const unsigned char*)input, length, md);
     memcpy(output,md, HASH_SIZE);
 
@@ -251,13 +259,12 @@ void hash256(unsigned char *output, const char *input)
 
 bool IsValidBlock(struct bloc* newBlock, struct bloc* previousBlock)
 {
-    char hash_to_test[HASH_HEX_SIZE];
-    char hash[HASH_SIZE];
-    char bloc_string[BLOCK_STR_SIZE];
-    printBlock(newBlock);
-    hash256(hash,toString(newBlock,bloc_string));
-    printf("bloc_string 1 : %s", bloc_string);
-    Hex_Hash(hash, hash_to_test);
+    char hash_to_test[HASH_HEX_SIZE]={0};
+    char hash[HASH_SIZE]={0};
+    char bloc_string[BLOCK_STR_SIZE]={0};
+    //hash256(hash,toString(newBlock,bloc_string));
+    //printf("bloc_string 1 : %s\n", bloc_string);
+    Hex_Hash(newBlock, hash_to_test);
     if (previousBlock->index + 1 != newBlock->index)
     {
         printf("\nBlock invalide : indexs invalide (%d + 1 != %d).\n", previousBlock->index, newBlock->index);
@@ -277,36 +284,38 @@ bool IsValidBlock(struct bloc* newBlock, struct bloc* previousBlock)
 }
 
 void calculHash(struct bloc* Bloc)
-{
-    
+{    
     Bloc->nonce = 0;
     char hash_hex[HASH_HEX_SIZE];
     char hash[HASH_SIZE];
-    //hashPrinter(hash_test,HASH_SIZE);
     char bloc_string[BLOCK_STR_SIZE];
-    hash256(hash, toString(Bloc,bloc_string)); // mise à jour du hash
+    //hash256(hash, toString(Bloc,bloc_string));
     
-    Hex_Hash(hash, hash_hex);
+    Hex_Hash(Bloc, hash_hex);
     strcpy(Bloc->Hash, hash_hex);
 
     while(!HashMatchesDifficulty(hash_hex))
     {
         Bloc->nonce++;
-        hash256(hash, toString(Bloc,bloc_string));  // mise à jour du hash
-        Hex_Hash(hash, hash_hex);       //Conversion du hash en hex    
+        //hash256(hash, toString(Bloc,bloc_string));  
+        Hex_Hash(Bloc, hash_hex);       //Conversion du hash en hex    
         printf("hex : %s\n", hash_hex);    
     }
-    strcpy(Bloc->Hash, hash_hex);
+    strcpy(Bloc->Hash, hash_hex);       // mise à jour du hash
+    //hash256(hash, toString(Bloc, bloc_string));
+    /*Hex_Hash(Bloc, hash_hex);
+    printf("Hash calcule dans fonction : %s\n", hash_hex);
+    printf("Hash du bloc dans fonction : %s\n", Bloc->Hash);*/
     return;
 }
 
-char *Hex_Hash(char *input, char *output)
+char *Hex_Hash(struct bloc *Bloc, char *output)
 {
 
-    char bloc_string[32];
-    strcpy(bloc_string, input);
+    char bloc_string[BLOCK_STR_SIZE];
+    toString(Bloc, bloc_string);
 
-    unsigned char hash_value[HASH_SIZE];
+    char hash_value[HASH_SIZE];
     hash256(hash_value, bloc_string);
     
     char buffer[3];
