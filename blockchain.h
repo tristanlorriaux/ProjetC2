2c79fb9db3d00a6a2cdf0e1416557e085c2531bd
tristanlorriaux <57456860+tristanlorriaux@users.noreply.github.com>
2020-03-28 18:49:10 +0100

    Update blockchain.h

--- blockchain.h
+++ blockchain.h
@@ -1,4 +1,4 @@
-#include "openssl/sha.h"
+#include "openssl/sha.h"    //Librairies pour le cryptage
 #include "openssl/crypto.h"
 #include <math.h>
 #include <stdio.h>
@@ -7,7 +7,7 @@
 #include <stdbool.h>
 
 #define N 200           //Taille max de la blockchain
-#define M 150           //taille max des messages
+#define M 150           //Taille max des messages
 #define DIFFICULTY 4    //Difficulté de la proof of work
 
 #define HASH_SIZE 32
@@ -27,24 +27,28 @@ struct bloc{
     char Hash[HASH_HEX_SIZE];
     int index;
     Donnee* donnee;
-    int nonce;
+    int nonce;      //utile pour la Pow
     struct bloc *lien;
 }*Genesis;
 
 void ajout_block(Donnee* message);
 
-
-//void hackChain(void);
 char *toString(struct bloc *blocks, char *str);
-void hashPrinter(unsigned char hash[], int length);
-int hashCompare(unsigned char *str1, unsigned char *str2);
+
 void printBlock(struct bloc *blocs);
+
 void printAllBlock(void);
+
 void init_Data(Donnee* data);
+
 bool HashMatchesDifficulty(char Hex[HASH_HEX_SIZE]);
+
 void hexToBinary(char input[HASH_HEX_SIZE], char output[BINARY_SIZE]);
+
 void hash256(unsigned char *output, const char *input);
+
 bool IsValidBlock(struct bloc* newBlock, struct bloc* previousBlock);
+
 void calculHash(struct bloc* Block);
+
 char *Hex_Hash(struct bloc *Bloc, char *output);
-//bool startsWith(const char *pre, const char *str);
